83b8663483ba50e9302166c1e2586687f23f8a97
SLYWalker93 <notorioustw@gmail.com>
2015-10-03 03:07:00 -0700

    test1
    stage 1 shit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #this will be our main file for story and logic
+print "Hello, young wanderer. I haven't seen the likes of you in my forest for some time."
+name=raw_input("What/'s your name?'")
+print "So, you are called %s." % (name)
+purpose=raw_input("What brings you here?")
+print "Ha, a quest of sorts, eh? I enjoy seeing the adventurer's spirit in humans. You have all grown quite boring, safe behind your city walls. But you're different, I can see it."
