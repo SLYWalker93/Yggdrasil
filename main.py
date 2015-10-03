ae7c8a3b3bc4ef713c10e59f4b57c3f63d26fc5d
SLYWalker93 <notorioustw@gmail.com>
2015-10-03 10:56:08 -0700

    Fun Stuff
    Geterdone!

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os
+from path1 import path_1
+from path1 import path_1_1
+from path1 import path_1_2
+from path1 import path_1_3
+from path1 import path_f_1
+from path1 import path_f_2
+ #importing path_1 function from path1.py
+#import path2
+#import path3
 #using import os for clearing screen on mac and linux
 
 #this will be our main file for story and logic
+os.system('clear')
 
 print "THE QUEST FOR THE SACRED WATER"
-
 
 print " "
 print "For generations, your clan, the Sentries of the Southern Village have safeguarded the Great Tree."
 print "People from all over the kingdom came to receive healing from the tree's sap."
-print "Tragedy has struck; raiders attacked the village, hoping to use the tree for themselves, and it was scorched and deformed from flaming projectiles, and many brave sentries lost their lives."
-print "Since then, the Great Tree's healing properties have waned, the sap turned bitter; all over the kingdom people grow sick with a new disease brought by the barbarians; medics and priests are unable to heal it."
+print "Tragedy has struck; raiders attacked the village, hoping to use the tree for themselves, and it was scorched and deformed from flaming projectiles and torches."
+print "Many brave sentries lost their lives, many your loved ones."
+print "Since then, the Great Tree's healing properties have waned, the sap turned bitter."
+print "All over the kingdom people grow sick with a new disease brought by the barbarians; medics and priests are unable to heal it."
 print "All attempts to heal the tree were in vain. The greatest mystics and most talented alchemists were powerless to remedy damage."
-print "Legend tells of a healing water, hidden away in a forgotten temple in the northernmost portion of the kingdom that can restore the tree to its former glory."
+print "Legend tells of a healing water, hidden away in a forgotten temple in the northernmost portion of the kingdom."
+print "Only it can restore the tree to its former glory."
 print "You are a young prodigy, who has the respect and admiration of the village for your bravery, and status as a Sentry."
 print "The task has fallen to you; the sentries have also fallen victim to this great plague, and so you must make the trek north, and save your home!"
 
 
 print " "
-go_on = raw_input("Continue to page 2?")
+go_on = raw_input("Continue to page 2? Enter 'y' for yes, 'n' to end story.")
 print "Enter 'y' for yes, 'n' to end story"
 if (go_on == "y"):
 
@@ -29,30 +41,69 @@ if (go_on == "y"):
     print " "
     print " "
 
-    print "You are sitting in your room, "
+    print "You are sitting in your room. Recent events have left you unsettled."
+    print "Raider attacks, recent diseases, and now even your own mother lies ill."
+    print "She calls to you, and you heed her call. You find her lying in bed, the sickness present in her face."
 
-    print "MOTHER:For centuries, our clan has safeguarded the tree of life. We have ensured that pilgrims from across the realm may benefit from its healing properties."
+    print " "
+
+    print "MOTHER:For centuries, our clan has safeguarded the tree of life."
+    print "We have ensured that pilgrims from across the realm may benefit from its healing properties."
     print "But, I, and all the sentries, have failed this day, my child."
     print "Your training is complete. Now, you must choose your weapon."
     print "The wilds are fraught with dangers. Go over to the chest, at the end of my bed."
     print "Now, choose, my child. What shall safegaurd you, on your mission?"
+    print " "
+    print " "
 
     choice=raw_input ("You have three choices: sword and shield, bow and dagger, or staff and tome. Choose wisely...")
     if choice=="sword and shield" or choice=="sword":
-        print "So, the blade. You always excelled in combat training. May it serve you well."
+        path_1() #prints out path 1's stories
     elif choice=="bow and dagger" or choice=="bow":
-        print "The stealth approach, eh? Take care not to let those sticky fingers get you into trouble!"
+        path_1_1()
     elif choice=="staff and tome" or choice=="staff":
-        print "Yes, of course. Your mind has always been your strongest asset. Take care not to shout. A mage's voice is their greatest weapon."
+        path_1_2()
     else:
-        print "My child, you must choose your weapon. Time is of the essence!"
+        path_1_3()
+    print "Now, go my child. Fly fast and true, to the north."
+    print "Your first challenge will be passing through the forest. Take care not to wander off the path."
+    print "Dangerous creatures reside there. Some of them savage things. Others command the languge of men, and can bewitch you."
+    print "I love you."
+    print " "
+    choice=raw_input ("You look on your mother one last time. Do you embrace her? Or do you turn and go, too proud to consider you may not see her again? Enter 'turn and go' or 'embrace.'")
+    if choice=="embrace":
+        print " "
+        print "You embrace your sick mother, kissing her on the cheek, then rise to face the challenges ahead of you."
+        print " "
+        print " "
+    elif choice=="turn and go":
+        print " "
+        print "You know your mother is ill, and it's too risky to endanger your own life. You turn to face the challenges ahead."
 
+        print " "
+        print " "
+
+    print "You are on the road now, a few miles away from your village."
+    print "The forest stretches before you like an ocean of green, ready to engulf the weak."
+    print "You take one last look at home before you carry on, your weapon clutched tightly."
+    print " "
     print " "
 
-    print "You must venture to the northern wastes, and find the lost temple our ancestors once safeguarded."
+    go_on = raw_input("Continue to page 3? Enter 'y' for yes, 'n' to end story.")
+    if (go_on == "y"):
 
-    print "It is there you will find the sacred life-giving water, which can restore the tree we hold so dear."
+        os.system('clear')
 
-    print "You will brave dangers and conquer challenges our people have not faced for a generation."
-
-    print ""
+print "THE FOREST"
+print " "
+print " "
+print " "
+print "The light grows dim as you venture deeper into the dense forest."
+print "You hurdle over dead logs, pass over streams, and sneak past slumbering bears."
+print "As the darkness grows, you notice a clearing in the distance, bathed in light."
+print "However, it is off the beaten path your family has shown you before."
+answer=raw_input("What will you do? Enter 'go into the light' or 'continue.'")
+if answer=="go into the light":
+    path_f_1()
+elif answer== "continue":
+    path_f_2()
---
+++ path1.py
@@ -0,0 +1,23 @@
+#path 1 story
+
+def path_1(): #this will print out path1 story
+    print " "
+    print "MOTHER: So, the blade. You always excelled in combat training. May it serve you well."
+def path_1_1():
+    print " "
+    print "MOTHER: The stealth approach, eh? Take care not to let those sticky fingers get you into trouble!"
+def path_1_2():
+    print " "
+    print "MOTHER: Yes, of course. Your mind has always been your strongest asset. Take care not to shout. A mage's voice is their greatest weapon."
+def path_1_3():
+    print " "
+    print "MOTHER: My child, you must choose your weapon. Time is of the essence!"
+def path_f_1():
+    print " "
+    print "You head off the beaten path, and get closer and closer to the light."
+    print "The clearing is covered in grass and flowers, almost heavenly in appearance."
+    print "As you step into the clearing, you see a small, winged creature, similar to a human."
+    print "It is a fairy! Will you approach it?"
+def path_f_2():
+    print " "
+    print " You ignore the light, heeding your mother's warning."
