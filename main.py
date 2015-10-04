dbaad73124326a4ce1e3c2489b50b1beb171df27
SLYWalker93 <notorioustw@gmail.com>
2015-10-03 22:46:08 -0700

    Big Changes, made a lot of junk
    YO

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from path1 import path_f_1_1
 from path1 import path_f_1_2
 from path1 import path_f_1_3
 from path1 import path_f_1_4
+from path1 import path_f_2_1
+from path1 import path_f_2_2
+from path1 import path_f_2_3
 
  #importing path_1 function from path1.py
 #import path2
@@ -72,7 +75,7 @@ elif (go_on == "y"):
     print " "
     print " "
 
-    choiceweapon=raw_input ("You have three choices: sword and shield, bow and dagger, or staff and tome. Choose wisely...")
+    choiceweapon=raw_input ("You have three choices: 'sword and shield,' 'bow and dagger,' 'or staff and tome.' Choose wisely...")
     if choiceweapon=="sword and shield":
         path_1_a() #prints out path 1's stories
     else:
@@ -89,6 +92,10 @@ elif (go_on == "y"):
     #    path_1_2()
     #else:
     #    path_1_3()
+    print " "
+    print "Your MOTHER takes a moment to take in the sight of you: a tear comes to her eye."
+    print "Though she may not admit it, she fears she may not see you again."
+    print " "
     print "Now, go my child. Fly fast and true, to the north."
     print "Your first challenge will be passing through the forest. Take care not to wander off the path."
     print "Dangerous creatures reside there. Some of them savage things. Others command the languge of men, and can bewitch you."
@@ -123,8 +130,6 @@ elif (go_on == "y"):
 
         print "THE FOREST"
         print " "
-        print " "
-        print " "
         print "The light grows dim as you venture deeper into the dense forest."
         print "You hurdle over dead logs, pass over streams, and sneak past slumbering bears."
         print "As the darkness grows, you notice a clearing in the distance, bathed in light."
@@ -148,7 +153,7 @@ elif (go_on == "y"):
     print " "
 
 go_on_3= raw_input("Continue to page 4? Enter 'y' for yes, 'n' to end story.")
-if (go_on_3 == "n"):
+if (go_on_3 =="n"):
     print "Thanks for playing!"
 
 elif (go_on_3=="y"):
@@ -160,6 +165,90 @@ elif (go_on_3=="y"):
     print " "
 if answer=="continue":
     print "You continue on the path ahead, ever wary of danger."
+    print "You venture further into the forest as the shadows grow, and eventually"
+    print "consume the woods. You begin to hear the calls of savage bests,"
+    print " the howls of wolves, and screeches of nocturnal birds on the hunt."
+    print "Fear begins to set in; though you are brave, the tales you heard in youth torture your imagination."
+    print "You begin to panic, and clamor through the dark, searching for shelter."
+    print "You finally find it; a small cave, elevated from the forest floor, atop a cliff."
+    print "You hoist your baggage and begin the climb, desperate to escape the cacophany of the forest."
+    print "You finally reach the top, breathe a sigh of relief, and set up camp, just outside the cave, safe."
+    print "Or so you think..."
+    print " "
+    print " "
+    print "Your fire grows dim, and the night winds cut through your blankets,"
+    print "But you endure, and finally fall asleep. Though it does not last."
+    print "Soon after you have slipped into the grasp of Morpheus, you feel a soft rumble on the ground."
+    print "It is coming from the cave, and grows ever larger."
+    print " "
 elif answer=="go into the light":
+    print " "
     print "After your encounter with the fairies, your mother's message hits home."
     print "You must be on guard at all times in the forest."
+    print " "
+    print " "
+    print "You venture further into the forest as the shadows grow, and eventually"
+    print "consume the woods. You begin to hear the calls of savage bests,"
+    print " the howls of wolves, and screeches of nocturnal birds on the hunt."
+    print "Fear begins to set in; though you are brave, the tales you heard in youth torture your imagination."
+    print "You begin to panic, and clamor through the dark, searching for shelter."
+    print "You finally find it; a small cave, elevated from the forest floor, atop a cliff."
+    print "You hoist your baggage and begin the climb, desperate to escape the cacophany of the forest."
+    print "You finally reach the top, breathe a sigh of relief, and set up camp, just outside the cave, safe."
+    print "Or so you think..."
+    print " "
+    print " "
+    print "Your fire grows dim, and the night winds cut through your blankets,"
+    print "But you endure, and finally fall asleep. Though it does not last."
+    print "Soon after you have slipped into the grasp of Morpheus, you feel a soft rumble on the ground."
+    print "It is coming from the cave, and grows ever larger."
+    print " "
+
+fightorflight=raw_input("What shall you do? 'stay and fight,' or 'flee?'Choose one...")
+
+os.system('clear')
+print " "
+print " "
+if fightorflight=="flee":
+    print "You flee the scene, leaving some of your gear behind in your excitement,"
+    print "back into the forest's maw."
+elif fightorflight=="stay and fight":
+    print "You prepare yourself, readying your weapon once again; you shall fear no creature!"
+if choiceweapon=="sword and shield":
+    path_f_2_1()
+if choiceweapon=="staff and tome":
+    path_f_2_2()
+if choiceweapon=="bow and dagger":
+    path_f_2_3()
+    print " "
+    print " "
+if choiceweapon=="sword and shield" or choiceweapon=="staff and tome":
+    print "Terror fills your body as you stare into the eyes of the creature."
+    print "It licks its chops and stares through to your soul."
+    print "To your surprise, despite having your weapon raised,"
+    print "it sits, looking at you with as much curiosity as rabid hunger."
+    print " "
+elif choiceweapon=="bow and dagger":
+    print "You draw your bow back and aim for the creature's head."
+    print "Sweat covers your hands, and you slip, a rare mistake."
+    print "You hit the creature in its shoulder, but it bounces right off."
+    print " "
+    print "WOLF: Oh, come now. You're better than sneaking about in the dark,"
+    print "my human friend. Your species' weaponry is as useless as it's always been."
+    print "Come out, and we can have, a friendly discussion."
+    print " "
+if choiceweapon=="bow and dagger":
+    stealthchoice=raw_input("What will you do? 'flee,' or 'face the wolf?''")
+if choiceweapon=="bow and dagger" and stealthchoice==("flee"):
+    print "You flee back into the woods, the wolf none the wiser, though you left your"
+    print "bag behind."
+elif choiceweapon=="bow and dagger" and stealthchoice==('face the wolf'):
+    print " "
+    print "You decide to face the creature, and jump down."
+    print "Terror fills your body as you stare into the eyes of the creature."
+    print "It licks its chops and stares through to your soul."
+    print "To your surprise, despite having your weapon raised,"
+    print "it sits, looking at you with as much curiosity as rabid hunger. "
+    print " "
+    print " "
+    
