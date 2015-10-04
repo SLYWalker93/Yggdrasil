b4696a7ed9aceb67f81a35a0dd76455ebfcdb018
SLYWalker93 <notorioustw@gmail.com>
2015-10-03 17:35:24 -0700

    HArdwork
    added a bunch of strings, etc., stll figuring shit out

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
-from path1 import path_1
+from path1 import path_1_a
 from path1 import path_1_1
 from path1 import path_1_2
 from path1 import path_1_3
 from path1 import path_f_1
-from path1 import path_f_2
+from path1 import path_f_1_1
+from path1 import path_f_1_2
+from path1 import path_f_1_3
+from path1 import path_f_1_4
+
  #importing path_1 function from path1.py
 #import path2
 #import path3
@@ -13,64 +17,84 @@ from path1 import path_f_2
 #this will be our main file for story and logic
 os.system('clear')
 
-print "THE QUEST FOR THE SACRED WATER"
+print "T H E  Q U E S T  F O R  T H E  S A C R E D  W A T E R"
 
 print " "
 print "For generations, your clan, the Sentries of the Southern Village have safeguarded the Great Tree."
 print "People from all over the kingdom came to receive healing from the tree's sap."
-print "Tragedy has struck; raiders attacked the village, hoping to use the tree for themselves, and it was scorched and deformed from flaming projectiles and torches."
-print "Many brave sentries lost their lives, many your loved ones."
-print "Since then, the Great Tree's healing properties have waned, the sap turned bitter."
-print "All over the kingdom people grow sick with a new disease brought by the barbarians; medics and priests are unable to heal it."
-print "All attempts to heal the tree were in vain. The greatest mystics and most talented alchemists were powerless to remedy damage."
+print "Tragedy has struck; raiders attacked the village, hoping to use the tree for themselves."
+print "The tree was scorched and deformed by flaming projectiles and torches."
+print "Numerous Sentries lost their lives, many of them your friends and loved ones."
+print "Since then, the Great Tree's healing properties have waned, and its sap turned bitter."
+print "All over the kingdom people grow sick with a new disease brought by the barbarians."
+print "The kingdom's medics and priests are unable to cure the disease, and need the Great Tree. "
+print "All attempts to heal the tree were in vain."
+print "The greatest mystics and most talented alchemists were powerless to remedy the damage."
 print "Legend tells of a healing water, hidden away in a forgotten temple in the northernmost portion of the kingdom."
 print "Only it can restore the tree to its former glory."
-print "You are a young prodigy, who has the respect and admiration of the village for your bravery, and status as a Sentry."
-print "The task has fallen to you; the sentries have also fallen victim to this great plague, and so you must make the trek north, and save your home!"
+print "You are a young prodigy among the Sentries."
+print "You have the respect and admiration of the village for your bravery, and come from a great line of warriors."
+print "The task has fallen to you!"
+print "the Sentries have also fallen victim to this great plague, and so you must make the trek north, and save your home!"
 
 
 print " "
 go_on = raw_input("Continue to page 2? Enter 'y' for yes, 'n' to end story.")
-print "Enter 'y' for yes, 'n' to end story"
-if (go_on == "y"):
+if (go_on=="n"):
+    print "Thanks for playing!"
+
+elif (go_on == "y"):
 
     os.system('clear')  # on linux / os x
+
 
     print "Welcome to Your Quest!"
 
     print " "
     print " "
 
-    print "You are sitting in your room. Recent events have left you unsettled."
-    print "Raider attacks, recent diseases, and now even your own mother lies ill."
+    print "You are sitting in your room."
+    print "It has been many months since the last raider attack."
+    print "The leader of the Sentries called a meeting last week, and it was decided,"
+    print "that you would undertake the great task, to seek out the lost Temple."
+    print "You've donned your family Armor, said farewell to your friends, and must say goodbye to your mother."
     print "She calls to you, and you heed her call. You find her lying in bed, the sickness present in her face."
 
     print " "
 
-    print "MOTHER:For centuries, our clan has safeguarded the tree of life."
+    print "MOTHER: There you are my child. For centuries, our clan has safeguarded the Great Tree."
     print "We have ensured that pilgrims from across the realm may benefit from its healing properties."
-    print "But, I, and all the sentries, have failed this day, my child."
-    print "Your training is complete. Now, you must choose your weapon."
+    print "But I, and all the Sentries, have failed in our duty."
+    print "Your training is complete. I see you've donned the Sentry's armor. It suits you."
+    print "Now, you must choose your weapon."
     print "The wilds are fraught with dangers. Go over to the chest, at the end of my bed."
     print "Now, choose, my child. What shall safegaurd you, on your mission?"
     print " "
     print " "
 
-    choice=raw_input ("You have three choices: sword and shield, bow and dagger, or staff and tome. Choose wisely...")
-    if choice=="sword and shield" or choice=="sword":
-        path_1() #prints out path 1's stories
-    elif choice=="bow and dagger" or choice=="bow":
-        path_1_1()
-    elif choice=="staff and tome" or choice=="staff":
-        path_1_2()
+    choiceweapon=raw_input ("You have three choices: sword and shield, bow and dagger, or staff and tome. Choose wisely...")
+    if choiceweapon=="sword and shield":
+        path_1_a() #prints out path 1's stories
     else:
-        path_1_3()
+        if choiceweapon=="bow and dagger":
+            path_1_1()
+        else:
+            if choiceweapon=="staff and tome":
+                path_1_2()
+            else:
+                path_1_3()
+    #elif choiceweapon=="bow and dagger" or "bow":
+
+    #elif choiceweapon=="staff and tome" or "staff":
+    #    path_1_2()
+    #else:
+    #    path_1_3()
     print "Now, go my child. Fly fast and true, to the north."
     print "Your first challenge will be passing through the forest. Take care not to wander off the path."
     print "Dangerous creatures reside there. Some of them savage things. Others command the languge of men, and can bewitch you."
     print "I love you."
     print " "
-    choice=raw_input ("You look on your mother one last time. Do you embrace her? Or do you turn and go, too proud to consider you may not see her again? Enter 'turn and go' or 'embrace.'")
+    choice=raw_input ("You look on your mother one last time. Do you embrace her? Or do you turn and go? Enter 'turn and go' or 'embrace.'")
     if choice=="embrace":
         print " "
         print "You embrace your sick mother, kissing her on the cheek, then rise to face the challenges ahead of you."
@@ -89,21 +113,53 @@ if (go_on == "y"):
     print " "
     print " "
 
-    go_on = raw_input("Continue to page 3? Enter 'y' for yes, 'n' to end story.")
-    if (go_on == "y"):
+    go_on_2= raw_input("Continue to page 3? Enter 'y' for yes, 'n' to end story.")
+    if (go_on_2 == "n"):
+        print "Thanks for playing!"
+
+    elif (go_on_2=="y"):
 
         os.system('clear')
 
-print "THE FOREST"
-print " "
-print " "
-print " "
-print "The light grows dim as you venture deeper into the dense forest."
-print "You hurdle over dead logs, pass over streams, and sneak past slumbering bears."
-print "As the darkness grows, you notice a clearing in the distance, bathed in light."
-print "However, it is off the beaten path your family has shown you before."
-answer=raw_input("What will you do? Enter 'go into the light' or 'continue.'")
-if answer=="go into the light":
-    path_f_1()
-elif answer== "continue":
-    path_f_2()
+        print "THE FOREST"
+        print " "
+        print " "
+        print " "
+        print "The light grows dim as you venture deeper into the dense forest."
+        print "You hurdle over dead logs, pass over streams, and sneak past slumbering bears."
+        print "As the darkness grows, you notice a clearing in the distance, bathed in light."
+        print "However, it is off the beaten path your family has shown you before."
+    answer=raw_input("What will you do? Enter 'go into the light' or 'continue.'")
+    if (answer=="continue"):
+        print "You ignore the light, heeding your mother's warning about the dangers of the forest."
+
+    elif answer=="go into the light":
+        path_f_1()
+        path_f_1_1()
+
+        if choiceweapon=="sword and shield":
+            path_f_1_2()
+        elif choiceweapon=="staff and tome":
+            path_f_1_3()
+        elif choiceweapon=="bow and dagger":
+            path_f_1_4()
+
+    print " "
+    print " "
+
+go_on_3= raw_input("Continue to page 4? Enter 'y' for yes, 'n' to end story.")
+if (go_on_3 == "n"):
+    print "Thanks for playing!"
+
+elif (go_on_3=="y"):
+
+    os.system('clear')
+
+    print "THE FOREST-PART II"
+    print " "
+    print " "
+if answer=="continue":
+    print "You continue on the path ahead, ever wary of danger."
+elif answer=="go into the light":
+    print "After your encounter with the fairies, your mother's message hits home."
+    print "You must be on guard at all times in the forest."
--- path1.py
+++ path1.py
@@ -1,14 +1,22 @@
 #path 1 story
 
-def path_1(): #this will print out path1 story
+def path_1_a(): #this will print out path1 story
     print " "
     print "MOTHER: So, the blade. You always excelled in combat training. May it serve you well."
+    print "It is called Frostbreaker. It's a little worn, but has protected our clan for generations."
 def path_1_1():
     print " "
-    print "MOTHER: The stealth approach, eh? Take care not to let those sticky fingers get you into trouble!"
+    print "MOTHER: Yes, your archery skills are unmatched. The dagger you hold belonged to your grandfather, the old rogue!"
+    print "Ha, if only he could see you now."
+    print "I see you like the stealth approach, eh? Take care not to let those sticky fingers get you into trouble!"
 def path_1_2():
     print " "
-    print "MOTHER: Yes, of course. Your mind has always been your strongest asset. Take care not to shout. A mage's voice is their greatest weapon."
+    print "MOTHER: Yes, of course. Your mind has always been your strongest asset."
+    print "That staff was taken from a powerful sorceror, and the tome was your grandmother's."
+    print "Your grandfather stole it from the Arcane University for her. Ha! If they could see you now."
+    print "Take care child, that you do not become drunk on the power of the arcane;"
+    print "you are skilled, but you toy with powerful magics."
+    print "Take care not to shout. A mage's voice is their greatest weapon."
 def path_1_3():
     print " "
     print "MOTHER: My child, you must choose your weapon. Time is of the essence!"
@@ -17,7 +25,41 @@ def path_f_1():
     print "You head off the beaten path, and get closer and closer to the light."
     print "The clearing is covered in grass and flowers, almost heavenly in appearance."
     print "As you step into the clearing, you see a small, winged creature, similar to a human."
-    print "It is a fairy! Will you approach it?"
-def path_f_2():
-    print " "
-    print " You ignore the light, heeding your mother's warning."
+    print "It is a fairy!"
+def path_f_1_1():
+    print "You approach the fairy cautiously. It looks up as you step forward, and lands on a flower."
+    print "Suddenly, the other flowers begin to rustle, and a din erupts in the clearing."
+    print "One hundred fairies appear in place of the flowers, and swarm towards you!"
+    print "Ready your weapon!"
+def path_f_1_2():
+    print "You swing your sword wildly and raise your shield, battering fairies left and right."
+    print "For every one you strike, five more attack you, scratching at your face, and trying to tear off your armor."
+    print "You go into a run, circling the path at your top speed, forcing the fairies to pursue."
+    print "They act as bees might, as a hive mind, condensing into a cloud of wings."
+    print "Just as they begin to gain on you, you turn and raise your shield, stopping the creatures dead in their flight path."
+    print "You sheathe your sword and continue on, bruised and exhausted, but alive."
+
+def path_f_1_3():
+    print "You recall your studies and cast a spell of inertia, stopping the angry creatures in their tracks."
+    print "Their eyes and mouths still move, and the creatures chatter rabidly."
+    print "You recognize their language, and are able to speak with them!"
+    print "The chief fairy claims you have disturbed their rest, and must pay a toll to pass their field."
+    print "You debate him thoroughly, finally agreeing to teach them the spell you cast on them."
+    print "You release the creatures from their bondage, and they peacefully gather around for the lesson, as fairies are known for their magical prowess."
+    print "As night begins to set in the fairies celebrate with you, sharing their libations and playing their music."
+    print "You sleep heartily amidst them, but when you wake the next morning, they have disappeared."
+    print "You return to your path, wondering whether the creatures had been there at all."
+    print "When you check your armor, you find a bag filled with coins; perhaps a prize won from some unlucky traveler, who couldn't speak fairy."
+
+def path_f_1_4():
+    print "You dodge the initial attack, and drop a smokebomb of your own design."
+    print "The creatures are blinded and some even drop from the smoke."
+    print "Swiftly you slip back into the forest, leaping through the trees."
+    print "The fairies are incensed, and head off in different directions, attempting to find you."
+    print "You wait until nightfall, and witness the creatures going in and out of a small log, hidden from view, incessntly."
+    print "One of them finally returns with a gold coin, which they pass around and laugh madly."
+    print "After some time they fall asleep, and you seize the oppurtunity."
+    print "You quickly sneak past the slumbering creatures, careful not to touch ground in the clearing."
+    print "You reach the log and find a veritable treasure trove, loot captured from less skilled travelers."
+    print "You qickly fill your pockets, careful not to overburden yourself."
+    print "You slip away just as one of the creatures comes to check the log, and disappear as they frenzy."
