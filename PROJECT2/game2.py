6ed3e3303f69c580830160dc9f5730d9a2da9491
SLYWalker93 <notorioustw@gmail.com>
2015-10-08 19:51:14 -0700

    Game2
    New game I’m practicing with

---
+++ PROJECT2/game2.py
@@ -0,0 +1,21 @@
+#testsnshit
+import os
+os.system('clear')
+
+print "F A N T A S Y T E S T"
+print " "
+print "N U M E R O  U  N  O!!"
+print " "
+
+age=raw_input("What is your age?")
+if age.isdigit():
+    print " "
+    print "So, your age is %s." %(age)
+    print "Wow, you're old!"
+    print " "
+else:
+    print "empty"
+name=raw_input("What is your name?")
+print " "
+if name.isalpha():
+    print "And your name is %s." %(name)
